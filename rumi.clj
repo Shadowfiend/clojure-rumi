fb4b01d4433a5ae0c73d00fec7bc0d87157a4e6a
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2008-12-23 16:21:04 -0500

    We are now drawing a single tile with number on it.
    Of course, the important part is that we actually have a function to
    generate the right QGraphicsItem proxy for the tile.

--- rumi.clj
+++ rumi.clj
@@ -1,6 +1,9 @@
 (ns clojure-rumi
-  (:import (com.trolltech.qt.gui QApplication QGraphicsView QGraphicsScene)
-           (com.trolltech.qt.core QCoreApplication)))
+  (:import (com.trolltech.qt.gui QApplication QGraphicsView QGraphicsScene
+                                 QGraphicsItem QBrush QPen QFont QFont$Weight
+                                 QColor QTextOption)
+           (com.trolltech.qt.core QCoreApplication QRectF Qt Qt$AlignmentFlag
+                                  Qt$Alignment)))
 
 (defn init []
   (QApplication/initialize (make-array String 0)))
@@ -13,11 +16,32 @@
      ~@rest
      (exec)))
 
+(defn tile [tile-data]
+  (proxy [QGraphicsItem] []
+    (paint [painter option widget]
+           (let [font (new QFont)]
+             (.setWeight font (.. QFont$Weight Bold value))
+             (.setFont painter font))
+           (let [color QColor/red]
+             (.setPen painter (new QPen color)))
+
+           (.drawRoundedRect painter 0 0 40 40 5 5)
+
+           (let [align (new Qt$Alignment 
+                            (.. Qt$AlignmentFlag AlignCenter value))
+                 option (new QTextOption align)]
+             (.drawText painter
+                        (.boundingRect this)
+                        (.toString (tile-data :number))
+                        option)))
+    (boundingRect [] (new QRectF 0 0 40 40))))
 
 (defn start []
   (qt4
    (let [app (QCoreApplication/instance)
-         rumi-canvas (new QGraphicsView (new QGraphicsScene))]
-     (.. rumi-canvas scene (addText "Hello world!"))
+         rumi-canvas (new QGraphicsView (new QGraphicsScene))
+         rumi-tile (tile { :number 5 })]
+
+     (.. rumi-canvas scene (addItem rumi-tile))
      (.show rumi-canvas))))
 
